8c7c7a2e5c2548366c17053f75d10734bf5363d2
Justin Kenney <justinkenney13@gmail.com>
2018-11-12 18:22:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
   get '/' do
     erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    params
   end
 end
